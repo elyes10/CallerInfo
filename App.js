aadbc5eb657c09c2802fbd4508edda04cc77960b
elyes10 <47626987+elyes10@users.noreply.github.com>
2024-12-03 17:58:46 +0100

    Initial commit

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import CallDetectorManager from 'react-native-call-detection';
 
 export default function App() {
+  useEffect(() => {
+    const callDetector = new CallDetectorManager(
+      (event, phoneNumber) => {
+        if (event === 'Incoming') {
+          // Show a popup with the phone number
+          Alert.alert('Incoming Call', `Phone number: ${phoneNumber}\nMessage: Hello!`);
+        } else if (event === 'Disconnected') {
+          console.log('Call Disconnected');
+        }
+      },
+      false, // Don't request the phone number (set true to get the phone number on Android)
+      () => {
+        console.log('Permission denied to access phone state');
+      },
+      {
+        title: 'Phone State Permission',
+        message: 'This app needs access to your phone state in order to react to incoming calls.',
+      }
+    );
+
+    // Cleanup when component is unmounted
+    return () => {
+      callDetector && callDetector.dispose();
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Call Detection Example</Text>
     </View>
   );
 }
